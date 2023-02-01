277a6fffe55ba3dbe47e7c821278325829072d1e
ldm0805 <leonardodimaggio94@gmail.com>
2023-02-01 15:52:45 +0100

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -8,9 +8,23 @@ createApp({
             language: ''
         }
     },
+    methods: {
+        addToDoItem() {
+            const data = {
+                todoItem: this.language
+            }
+            axios.post(this.apiUrl, data, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            }).then((response) => {
+                this.language = '';
+                this.todoList = response.data;
+            })
+        }
+    },
     mounted() {
         axios.get(this.apiUrl).then((response) => {
             this.todoList = response.data;
+
         });
     }
 }).mount('#app');
